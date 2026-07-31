a82fff0c22607004a862e769468e4cf4afd0caf8
encoderit-touhid <touhid@encoderit.net>
2026-07-31 20:26:49 +0600

    work

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Book, Author
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_filter = ("rating", "is_bestselling", "author")
+    list_display = ("title", "author", "is_bestselling", "rating", "slug")
+    # readonly_fields=("slug",)
+    prepopulated_fields={"slug":("title",)}
+    
+admin.site.register(Book,BookAdmin)
+admin.site.register(Author)
--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,24 +1,33 @@
 from django.db import models
-from django.core.validators import MaxLengthValidator,MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name  = models.CharField(max_length=255)
+    def __str__(self):
+     return f"first_name: {self.first_name}, last_name: {self.last_name}"  
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    rating = models.IntegerField(validators=[MinValueValidator(1),MaxLengthValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    # author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
     is_bestselling=models.BooleanField(default=False)
-    slug = models.SlugField(default='', null=False, db_index=True)
+    # slug = models.SlugField(default='', editable=False, blank=True, null=False, db_index=True)
+    slug = models.SlugField(default='',  null=False, db_index=True)
     
     def get_single_permalink(self):
         return reverse('show', args=[self.slug])
     
-    def save(self, *args, **kwargs):
-        self.slug=slugify(self.title)
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     self.slug=slugify(self.title)
+    #     super().save(*args, **kwargs)
     
     def __str__(self):
-        return f"title: {self.title}, rating: {self.rating}, author: {self.author}, is_bestselling: {self.is_bestselling}"
+        # return f"title: {self.title}, rating: {self.rating}, author: {self.author}, is_bestselling: {self.is_bestselling}"
+         return f"title: {self.title}, rating: {self.rating},  is_bestselling: {self.is_bestselling}"
 
     
